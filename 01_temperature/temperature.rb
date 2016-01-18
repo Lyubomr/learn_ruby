87daefe21aac0f2d046c7814a9f911f0f8389d48
Lyubomr <s.lyubomyr@gmail.com>
2016-01-18 13:56:19 -0500

    Added ctof method

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,5 +1,9 @@
 
-# Created method that converts given fahrenheit temperature into celsius. 
+# Created method that converts given fahrenheit temperature into celsius.
 def ftoc(temp)
   (temp - 32) * 5.0/9.0
+end
+# Method used to convert celsius to fahrenheit. 
+def ctof(temp)
+  (temp * 1.8) + 32
 end
