1645c505dcb28e5951484871d64738b589e0ca4d
Lyubomr <s.lyubomyr@gmail.com>
2016-01-18 15:28:28 -0500

    Added Sum method

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,12 +1,17 @@
+
 # Addition method
 def add(x, y)
   x.to_i
   y.to_i
   x + y
 end
-
+#Subtract method
 def subtract(x, y)
   x.to_i
   y.to_i
   x - y
+end
+#Sum of an array method
+def sum(value)
+  value == [] ? 0 : value.inject(:+)
 end
